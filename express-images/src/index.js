6628e9d644ae9a8bd64d88f99d6aaf62a05a8da6
frederic <frederiic@outlook.com>
2020-05-24 12:40:33 +0200

    Step 2b, added a docker image wrapping an express server

--- express-images/src/index.js
+++ express-images/src/index.js
@@ -1,4 +1,41 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send("Hello RES labo ");
+});
+
+app.get('/test', function (req, res) {
+    res.send("Hello RES  - test is working ");
+});
+app.get('/generateNumbers', function (req, res) {
+    res.send(generateRandomNumbers());
+});
+
+function generateRandomNumbers() {
+    var numberOfNumbers = chance.integer({
+        min: 3,
+        max: 10
+    });
+
+    var numbers = [];
+    for (var i = 0; i < numberOfNumbers; ++i) {
+        var integerNb = chance.integer({ min: 0, max: 100 });
+        var doubleNb = 
+        numbers.push({
+            integer:integerNb,
+            floating:chance.floating({ min: 0, max: 100, fixed: 8 })
+        });
+        
+    }
+    console.log(numbers);
+    return numbers;
+
+}
+
+app.listen(3000, function () {
+    console.log("Acceptin HTTP requests on port 3000");
+});
